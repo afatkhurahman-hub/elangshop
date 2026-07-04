b254a7e315c1155f9f3174923a9e2ff70e5420df
Alif <a.fatkhurahman@students.amikom.ac.id>
2026-07-04 23:38:27 +0700

    elangshop login

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+// Mengambil variabel dari .env, jika tidak ada (saat build di Vercel) berikan string palsu sebagai backup
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://ktpbpmowaveksdmijwhr.supabase.co";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.dummykeyplaceholder";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
